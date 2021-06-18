97726633a42e48bc3ace18b3700d794bb6f05c25
muba-1999 <mukobam4@gmail.com>
2021-06-18 12:06:32 -0700

    priniting numbers separated by commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
 	for (n = 0; n < 10; n++)
 	{
 		putchar((n % 10) + '0');
-		putchar(44);
-		putchar(32);
+		if (n != 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
 	putchar('\n');
 
