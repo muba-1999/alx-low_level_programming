e3b48113084e069f6539484cebe7ed69a533513c
muba-1999 <mukobam4@gmail.com>
2021-06-18 07:00:27 -0700

    code to get the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,6 @@
 
 /* more headers goes there */
 
-
-
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
@@ -14,26 +12,23 @@
  */
 int main(void)
 {
-	int n, lastDigit;
+	int n;
 
 	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-	n = rand() - RAND_MAX / 2;	
-	lastDigit = n % 10;
-
-	if (lastDigit == 0)
+	if ((n % 10) == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	else if (lastDigit > 5)
+	else if ((N % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 
 	return (0);
-
 }
