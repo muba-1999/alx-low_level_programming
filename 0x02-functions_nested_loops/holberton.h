1594b2dc19b297f1ae5e94eb84d7c4119f45934c
muba-1999 <mukobam4@gmail.com>
2021-06-22 11:20:59 -0700

    added new function to header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,9 +1,12 @@
 #include <unistd.h>
+#ifndef PRINT_ALPHABET_X10(void)
+#define PRINT_ALPHABET(void)
+void print_alphabet_x10(void)
+void print_alphabet(void)
 
+#endif
 
-
-/**
- *_putchar - writes the character c to stdout
+ /**_putchar - writes the character c to stdout
  *@c: The character to print
  *
  * Return: On success 1.
@@ -18,3 +21,18 @@ int _putchar(char c);
  * Return: void
  */
 void print_alphabet(void);
+
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: Void
+ */
+void print_alphabet_x10(void);
+
+/**
+ * _islower - checks is a letter is lower case
+ *
+ * Return: 1 for true
+ */
+int _islower(int c);
