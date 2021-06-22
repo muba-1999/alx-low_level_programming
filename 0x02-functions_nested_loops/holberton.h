f66972d86ca7deed7cda927cc49c0a756a7d9006
muba-1999 <mukobam4@gmail.com>
2021-06-22 09:46:18 -0700

    file containing function prototype

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -9,10 +9,7 @@
  * Return: On success 1.
  *On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
                                                                                                                       
                                                                                                                
 /**
@@ -20,13 +17,4 @@ int _putchar(char c)
  *
  * Return: void
  */
-void print_alphabet(void)
-{
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-}
+void print_alphabet(void);
