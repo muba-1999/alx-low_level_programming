9be8b972db1dcdc64fe904f90da56f474597451f
muba-1999 <mukobam4@gmail.com>
2021-06-22 06:14:50 -0700

    functions that prints the alphabet

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -9,7 +9,22 @@
  * Return: On success 1.
  *On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char *c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+#include <stdio.h>                                                                                                                      
+
+                                                                                                                                        
+
+void print_alphabet(void)                                                                                                             
+{
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)                                                                                                     
+	{                                                                      
+		_putchar(i);                                                        
+	}
+       _putchar('\n');	
 }
