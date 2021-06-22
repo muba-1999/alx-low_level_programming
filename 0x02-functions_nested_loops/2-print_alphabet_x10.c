0e9beb4ef03e09b0cfff3975e8d8b4273aed9e3c
muba-1999 <mukobam4@gmail.com>
2021-06-22 10:06:37 -0700

    code to print the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10()
+{
+	int i;
+	char j;
+
+	while (i < 10)
+	{
+		for (j = 'a'; j <= 'z'; i++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
