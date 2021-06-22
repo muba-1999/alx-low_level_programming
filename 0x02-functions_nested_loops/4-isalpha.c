5adf62bddb14f64cae23457dbec875c865a9ac30
muba-1999 <mukobam4@gmail.com>
2021-06-22 12:42:17 -0700

    checks an argument if it a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "holberton"
+
+/**
+ * _isalpha - checks id an argument is a letter
+ * @c - writes the value to the stdout
+ *
+ * Return: on True 1
+ * Return: on False 0
+ */
+int _isalpha(int c)
+{
+	if ((c == 'A' && c == 'Z') || (c == 'a' && c == 'z'))
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
