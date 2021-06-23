a9ee1eac669bdcd02752e2c4f449ab165ce586ed
muba-1999 <mukobam4@gmail.com>
2021-06-23 09:54:45 -0700

    code that checks if a number is positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_sign: checks if a nuber is positive or negative
+ * n: The character to be checked
+ *
+ * Return: 1 when positive
+ * 0 when not positive or negative
+ * -1 when negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0):
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
