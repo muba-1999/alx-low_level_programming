ce1b3b8a9bf78906e1738f432f95a223ad6b338c
muba-1999 <mukobam4@gmail.com>
2021-06-23 09:42:39 -0700

    code that checks if value passed is an alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,21 +5,16 @@
  * @c - writes the value to the stdout
  *
  * Return: on True 1
- * Return: on False 0
+ * on False 0
  */
 int _isalpha(int c)
 {
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		if ((c == i) || (c == 'A' && 'Z'))
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else 
+	{
+		return (0);
 	}
 }
