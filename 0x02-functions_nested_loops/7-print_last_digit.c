97ecb85e91546826e336ec93c9c790d2848e5b1f
muba-1999 <mukobam4@gmail.com>
2021-06-23 10:31:42 -0700

    code that print last digit of a value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints tha last digit of a value
+ * @n: The value to be printed
+ *
+ * Return: n on Success
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	return (last);
+}
