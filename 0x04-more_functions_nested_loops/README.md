fa05f75335fa11456e822daad405984593e13992
muba-1999 <mukobam4@gmail.com>
2021-06-23 22:57:24 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+project on functions and nested loops
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _isupper(int c);
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */
