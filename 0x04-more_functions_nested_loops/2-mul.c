7ac2533f1e5b458f411be53ca9e8e3c4bce8dd30
muba-1999 <mukobam4@gmail.com>
2021-06-25 11:31:07 -0700

    multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - start point
+ * @a: first number
+ * @b: second number
+ *
+ * Return: value of a and b
+ */
+int mul(int a, int b)
+{
+	int total;
+
+	total = a * b;
+	return (total);
+}
