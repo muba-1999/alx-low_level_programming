e1cc8a19617700ee4898a9689190e6aba0f7cd61
muba-1999 <mukobam4@gmail.com>
2021-06-25 11:43:49 -0700

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_munbers - prints numbers
+ *
+ * Return: nothing void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
