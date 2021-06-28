00219ab0d4d389f2b1cf2b8da2e07710f2cab273
muba-1999 <mukobam4@gmail.com>
2021-06-28 10:28:00 -0700

    updated code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,23 +9,23 @@
  */
 void puts_half(char *str)
 {
-	int i;
+	int i, j;
 	int len = 0;
 
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	for (i = len / 2; i < len; i++)
+	if (len % 2 == 0)
 	{
-		if (len % 2 == 0)
+		for (j = len / 2; j < len; j++)
 		{
-			_putchar(str[i]);
+			_putchar(str[j]);
 		}
-		else
-		{
-			_putchar(str[i - 1]);
-		}
+	}
+	for (i = (len - 1) / 2; i < len; i++)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
