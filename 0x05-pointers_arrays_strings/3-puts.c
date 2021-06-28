80941af473191af8973451ccde06545e69e8c8dc
muba-1999 <mukobam4@gmail.com>
2021-06-28 01:52:26 -0700

    function that prints a sreing

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <string.h>
 
 /**
  * _puts - prints a string to stdout
@@ -6,7 +7,7 @@
  *
  * Return: void
  */
-void _puts(char *str);
+void _puts(char *str)
 {
 	write(1, str);
 }
