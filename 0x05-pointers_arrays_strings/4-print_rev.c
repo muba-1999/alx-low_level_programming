502bdc49a022be6a20a4c37b69bac84e287e3e2e
muba-1999 <mukobam4@gmail.com>
2021-06-28 02:59:50 -0700

    printing a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,13 @@ void print_rev(char *s)
 {
 	int i;
 	int len = 0;
-	
+
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 	for (i = len -1; i >= 0; i--)
 	{
-		printf("%c\n", s[i]);
+		printf("%c", s[i]);
 	}
 }
