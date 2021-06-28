66235210ff53b5e8cf28347e6561afc58c1b4d24
muba-1999 <mukobam4@gmail.com>
2021-06-28 11:25:46 -0700

    conerting a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _atoi - start point
+ * @s: character to print
+ *
+ * Return: s for true
+ * 0 on false
+ */
+int _atoi(char *s)
+{
+	int res = 0;
+	int i = 0;
+	int sign = 1;
+
+	if (s == '\0')
+	{
+		return (0);
+	}
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	for (i; s[i] != '\0'; i++)
+	{
+		if (s[i] => '0' && s[i] <= '9')
+		{
+			res = res * 10 + s[i] - '0';
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (res * sign);
+}
