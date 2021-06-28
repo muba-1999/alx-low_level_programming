8c10cc2766747dbef227c026fe7e01979c716944
muba-1999 <mukobam4@gmail.com>
2021-06-28 03:29:47 -0700

    reverse string function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,9 @@
  */
 void rev_string(char *s)
 {
-	int len = 0;
 	int i;
 
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	for (i = len - 1; i >= 0; i--)
+	for (i = strlen(s) - 1; i >= 0; i--)
 	{
 		*s = s[i];
 	}
