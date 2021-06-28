a9f8ead49fa4472f4691fd671ce4fd9673b63b96
muba-1999 <mukobam4@gmail.com>
2021-06-28 04:58:25 -0700

    function printing characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * puts2 - starting point
+ * @str - character to bet checked
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	char c;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		c = str[i];
+	}
+}
