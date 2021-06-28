f221caf01952633034006b1bbefe31f0d152bead
muba-1999 <mukobam4@gmail.com>
2021-06-28 08:09:59 -0700

    copying a string including the null character

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - coies a string
+ * @dest: first character
+ * @src: second character
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src);
+{
+	int i = 0;
+	int j;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = '\0';
+	}
+}
