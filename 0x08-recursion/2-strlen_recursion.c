5db4e6f99b9fc1e9c63b6270513f3346e40da80c
muba-1999 <mukobam4@gmail.com>
2021-07-07 00:51:40 -0700

    finds length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - counts the length of a string
+ * @s: character to print
+ * 
+ * Return: 1 if @s == NULL byte
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return (i);
+	}
+	else
+	{
+		i++;
+	}
+	_strlen_recursion(s, i)
+}
