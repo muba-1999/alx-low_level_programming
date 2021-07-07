11d81e5e39664d30b56c6ba403b9ef42219294ac
muba-1999 <mukobam4@gmail.com>
2021-07-07 00:57:34 -0700

    finds length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,15 +8,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
-		return (i);
+		return (0);
 	}
 	else
 	{
-		i++;
+		return (1 + _strlen_recursion(s + 1);
 	}
-	_strlen_recursion(s, i);
 }
