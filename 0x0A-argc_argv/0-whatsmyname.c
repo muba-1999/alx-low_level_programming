787f8dd640a54584bb37f62686c2e338a776a793
muba-1999 <mukobam4@gmail.com>
2021-07-12 03:12:17 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count sotres number of commands passed by the user
+ * @argv: argument vector 1 dimension array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	printf("\n");
+
+	return (0);
+}
