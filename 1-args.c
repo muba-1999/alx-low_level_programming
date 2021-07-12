b0f50c612f88272e405260cf29d05b8a523f40b8
muba-1999 <mukobam4@gmail.com>
2021-07-12 03:25:12 -0700

    prints number of arguments passed

---
+++ 1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: argument vector array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
