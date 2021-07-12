3e6fce65d2a7e1172fcee0c26678493bed701ff6
muba-1999 <mukobam4@gmail.com>
2021-07-12 10:17:34 -0700

    multiplying two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument counter 
+ * @argv: 1 dimessional array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", product);
+	}
+
+	return (0);
+}
