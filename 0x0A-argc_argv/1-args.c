056e34eec9499b50b42bd4b44c5ade2381262716
muba-1999 <mukobam4@gmail.com>
2021-07-12 04:08:07 -0700

    counts the number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: argument array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
