192b861ed8a8086def8c1f4b34bed3b41fcab40b
muba-1999 <mukobam4@gmail.com>
2021-07-13 11:06:23 -0700

    printing an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *ar;
 
-	ar = malloc((size + 1) * sizeof(char));
+	ar = malloc((size - 1) * sizeof(char));
 	if (ar == NULL)
 	{
+		_putchar("failed to allocate memory");
 		return (NULL);
 	}
 	while (i < size)
