17b34be380715d10f3ddc3c53797335fa074a054
muba-1999 <mukobam4@gmail.com>
2021-07-13 13:48:44 -0700

    updated code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * str_concat - concatenates two strings
@@ -9,12 +10,13 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3 = (char *)0;
+	char *s3;
 	int i = 0;
 	int j = 0;
 	int k = 0;
 	int len = 0;
 
+	s3 = malloc(strlen(s1 + s2 + 1) * sizeof(char));
 	while (s1[i] != '\0')
 	{
 		s3[k] = s1[i];
@@ -28,11 +30,5 @@ char *str_concat(char *s1, char *s2)
 		k++;
 	}
 	s3[k] = '\0';
-	while (s3[len] != '\0')
-	{
-		len++;
-	}
-	s3[k] = '\0';
-	s3 = malloc((len + 1) * sizeof(int));
 	return (s3);
 }
