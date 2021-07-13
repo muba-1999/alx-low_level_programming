5c7eae01eb3615194da884a628f3a5d916cab2fe
muba-1999 <mukobam4@gmail.com>
2021-07-13 11:20:37 -0700

    printing an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,11 +14,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *ar;
 
-	if (size == 0)
+	ar = malloc(size * sizeof(char));
+	if (atoi(ar) == 0)
 	{
 		return (NULL);
 	}
-	ar = malloc(size * sizeof(char));
 	while (i < size)
 	{
 		ar[i] = c;
