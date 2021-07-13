083caf6bcdc7816876891c77fb4a2cf7d66f38ec
muba-1999 <mukobam4@gmail.com>
2021-07-13 12:35:12 -0700

    function to duplicate a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,14 +11,19 @@
 char *_strdup(char *str)
 {
 	char *str2;
-	size_t i = 0;
+	int i = 0;
+	int len = 0;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	str2 = malloc(strlen(str) * sizeof(char));
-	while (i < strlen(str))
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	str2 = malloc((len + 1) * sizeof(char));
+	while (i < len)
 	{
 		str2[i] = str[i];
 		i++;
