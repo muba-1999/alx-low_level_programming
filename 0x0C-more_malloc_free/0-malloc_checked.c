a774122b39d31bacdef73e030eaec1d8eb8c4517
muba-1999 <mukobam4@gmail.com>
2021-07-15 01:17:52 -0700

    function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - assigns memory
+ * @b: character to be assigned memory to
+ *
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
