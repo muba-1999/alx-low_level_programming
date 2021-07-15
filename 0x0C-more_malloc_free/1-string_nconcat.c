5eaa667ec5845ca1ca4210dd0d42788cf21919cf
muba-1999 <mukobam4@gmail.com>
2021-07-15 11:20:01 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	int size = n;
 	int i, j;
-	int len1. len2;
+	int len1, len2;
 
 	if (s1 == NULL)
 	{
@@ -25,8 +25,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	for (len1 = 0; s1[len1] != '\0'; len1++);
-	for (len2 = 0; s2[len2] != '\0'; len2++);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{}
 	if (size >= len2)
 	{
 		size = len2;
