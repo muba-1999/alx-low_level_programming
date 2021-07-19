b55bb94e67da851551af5a4e2995870d59f9f4a3
muba-1999 <mukobam4@gmail.com>
2021-07-19 11:38:07 -0700

    code to initialize a struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - Entry point
@@ -12,6 +13,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
