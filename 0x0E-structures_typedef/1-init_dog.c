153a4d0588916a5c07c9321472780ef5f11f3134
muba-1999 <mukobam4@gmail.com>
2021-07-19 11:31:03 -0700

    code to initialize a struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,6 +3,7 @@
 /**
  * init_dog - Entry point
  * struct dog - contains information about the dog
+ * @d: pointer to struct
  * @name: the dogs name
  * @age: dogs age
  * @owner: dogs owner
@@ -11,7 +12,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
