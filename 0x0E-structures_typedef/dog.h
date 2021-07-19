67f47500acf994531528c5b48ca86223e0eb356a
muba-1999 <mukobam4@gmail.com>
2021-07-19 10:59:43 -0700

    updated code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - stracture containing dog details
+ * @name: dogs name
+ * @age: the dogs age
+ * @owner: the dogs owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
