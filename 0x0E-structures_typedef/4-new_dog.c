47965087986e57b2629ee048ff7412166a343c71
muba-1999 <mukobam4@gmail.com>
2021-07-19 12:27:51 -0700

    creating a new struct variable

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Entry point
+ * @name: name of the dog
+ * @age: dogs age
+ * @owner: name of dogs owner
+ *
+ * Return: dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	dog->name = name;
+	dog->age = age;
+	gog->owner = owner;
+	return (dog);
+}
