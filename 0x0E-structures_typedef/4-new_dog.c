3a7dbaf381cbdb93f2d6956c6c9f68a57729f631
muba-1999 <mukobam4@gmail.com>
2021-07-19 12:45:56 -0700

    creating a new struct variable

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,13 +14,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(dog_t));
+	dog = malloc(sizeof(dog));
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
 	dog->name = name;
 	dog->age = age;
-	gog->owner = owner;
+	dog->owner = owner;
 	return (dog);
 }
