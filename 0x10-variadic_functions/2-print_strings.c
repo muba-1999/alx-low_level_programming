665ecd6d1fb846389b29d063e30bb51dca4deb7e
muba-1999 <mukobam4@gmail.com>
2021-07-22 03:23:10 -0700

    function that prints strings passed to the function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,8 +17,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(arg, char**));
-		if (n[i] == NULL)
+		printf("%s", va_arg(arg, char *));
+		if (va_arg(arg, char *) == NULL)
 		{
 			printf("nil");
 		}
