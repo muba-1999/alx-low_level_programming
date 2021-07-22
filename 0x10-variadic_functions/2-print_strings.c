01778b0b65bae0e5877535e89db421463c152203
muba-1999 <mukobam4@gmail.com>
2021-07-22 03:19:43 -0700

    function that prints strings passed to the function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings passed to the function
+ * @separator: characters to print between the strings
+ * @n: size of the arguments passed
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(arg, char**));
+		if (n[i] == NULL)
+		{
+			printf("nil");
+		}
+		if (separator != NULL && i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
