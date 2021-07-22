61150f4f42610ba2fe318dc40f21e2dc418e36ec
muba-1999 <mukobam4@gmail.com>
2021-07-22 14:54:04 -0700

    updated code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,49 +10,47 @@
  */
 void print_all(const char * const format, ...)
 {
+	const char *s;
 	int integer;
 	float flo;
-	char ch;
+	int ch;
 	char *str;
 	va_list arg;
 
 	va_start(arg, format);
-	while (format)
+	for (s = format; *s != '\0'; s++)
 	{
-		switch (*format)
+		switch (*s)
 		{
 			case 'i':
 				integer = va_arg(arg, int);
 				printf("%i", integer);
-				format++;
+				s++;
 				break;
 			case 'f':
-				flo = va_arg(arg, float);
-				printf("f", flo);
-				format++;
+				flo = va_arg(arg, double);
+				printf("%f", flo);
+				s++;
 				break;
 			case 'c':
-				ch = va_arg(arg, char);
-				printf("c", ch);
-				format++;
+				ch = va_arg(arg, int);
+				printf("%i", ch);
+				s++;
 				break;
 			case 's':
 				str = va_arg(arg, char *);
 				printf("%s", str);
-				format++;
 				break;
 			default:
-				printf("%s", *format);
+				printf("%s", s);
 		}
-		if (str == NULL)
-		{
-			printf("nil");
-		}
-		if (*format != '\0' && (*format == 'i || *format == 'f' || *format == 'c' || *format == 's'))
+		if (*s != '\0' && (*s == 'i' || *s == 'f' || *s == 'c' || *s == 's'))
 		{
 			printf(",");
 		}
-		format++;
+		if (s == NULL)
+		{
+			printf("nil");
+		}
 	}
-	va_end(arg);
 }
