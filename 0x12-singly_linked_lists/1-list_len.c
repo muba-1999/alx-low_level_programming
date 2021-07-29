06ec7f48ea515eb45844794e1e22a841a3745ea9
muba-1999 <mukobam4@gmail.com>
2021-07-29 04:35:22 -0700

    updated code

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-#include "lists.h"
 
+
+/**
+ * list_len - prints the length of list_t
+ * @h: head node 
+ * Return: length
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
