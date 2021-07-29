db28591a22007039b82194544cab40c4d02f41e3
muba-1999 <mukobam4@gmail.com>
2021-07-29 07:07:30 -0700

    adds node to the front

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - adds a node to the beginning of a linked list
+ * @head: double pointer to a lined list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	tmp->str = strdup(str);
+	if (tmp->str == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
+	tmp->len = strlen(tmp->str);
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
+}
