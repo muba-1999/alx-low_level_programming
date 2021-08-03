985c9a4e8197291825f71cbc133177bb82880ad7
muba-1999 <mukobam4@gmail.com>
2021-08-03 03:52:03 -0700

    initial commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,14 +13,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 1;
 
 	if (head == NULL)
-	{
 		return (NULL);
-	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	new->n = n;
 	new->next = NULL;
 	if (idx == 0)
