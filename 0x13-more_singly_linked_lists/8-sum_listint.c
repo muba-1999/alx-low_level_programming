f66f6095cc85f1810184ca176eb969dac57db3d9
muba-1999 <mukobam4@gmail.com>
2021-08-03 03:21:56 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * sum_listint - sums thr data of listint_t
+ * @head: reference node
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
