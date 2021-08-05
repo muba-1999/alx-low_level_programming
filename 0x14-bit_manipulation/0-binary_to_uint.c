18dbdef5fd72a59caa56858b2f36bb2b00ca2b64
muba-1999 <mukobam4@gmail.com>
2021-08-05 10:11:05 -0700

    updated code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,15 +6,15 @@
  *@b: pointer to the string
  *Return: unsigned int
  */
-unsigned int binary_to_uint( const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int k = 1;
-	unsigned int i=0;
+	unsigned int i = 0;
 	int c;
 	unsigned int len;
 
 	len = strlen(b);
-	for (c = len-1; c >= 0; c--)
+	for (c = len - 1; c >= 0; c--)
 	{
 		if (b[c] != '0' && b[c] != '1')
 		{
