57a5cfabb00b7c9c3612fc8f205e0b278838be01
muba-1999 <mukobam4@gmail.com>
2021-08-05 11:14:28 -0700

    initial commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+i
+/**
+ * get_bit - gets the bit of a number
+ * at a given index
+ * @n: integer to get bit value from
+ * @index: index of n
+ * Return: bit at index
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int size;
