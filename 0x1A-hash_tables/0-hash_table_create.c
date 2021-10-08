ae87d77c5121e12530ee8989ba39faef5e6fd694
muba-1999 <mukobam4@gmail.com>
2021-10-08 14:00:09 -0700

    changed code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash;
+	unsigned long int i;
 
-	hash = malloc(size * sizeof(hash_table_t));
+	hash = malloc(sizeof(hash_table_t));
 
 	if (hash == NULL)
 	{
 		return (NULL);
 	}
+	hash->size = size;
+	hash->array = malloc(size * sizeof(hash_table_t *));
+	if (hash->array == NULL)
+	{
+		free(hash);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		hash->array[i] == NULL;
+	}
 	return (hash);
-	free(hash);
 }
